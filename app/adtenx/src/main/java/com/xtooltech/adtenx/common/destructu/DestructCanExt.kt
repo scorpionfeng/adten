9a6897471c42bf4ff4ad9404769f6eda83b393f2
phoneix <feng.ke@xtooltech.com>
2020-04-20 15:01:03 +0800

    1.优化扩展can解析

--- app/adtenx/src/main/java/com/xtooltech/adtenx/common/destructu/DestructCanExt.kt
+++ app/adtenx/src/main/java/com/xtooltech/adtenx/common/destructu/DestructCanExt.kt
@@ -1,5 +1,6 @@
 package com.xtooltech.adtenx.common.destructu
 
+import com.xtooltech.adtenx.util.parse2BizSingle
 import java.lang.Exception
 
 
@@ -41,6 +42,76 @@ class DestructCanExt :DestructBiz{
     }
 
     override fun parseTrobCode(cmd: Byte, data: List<ByteArray?>): Pair<Int, List<String>> {
-        return Pair(0, listOfNotNull())
+        var isSingle=true
+        var amount_code=0
+        var index_flag=0
+        var troCodeTextList= mutableListOf<String>()
+        var troCodeDataList= mutableListOf<Byte>()
+        data?.forEachIndexed loop@{
+                index,item->
+            item?.apply {
+
+                takeIf { index == 0 }?.apply {
+                    index_flag= item.indexOf(0x10)
+                    takeIf { index_flag>-1 }?.let { isSingle=false }
+                }
+
+                takeIf { isSingle }?.apply {/** 单帧 */
+//               val data_parsed= parse2BizSingle(this)
+                val sureAnsIndex = indexOf(cmd.plus(0x40.toByte()).toByte())
+                    takeIf { sureAnsIndex == -1 }?.apply { return@loop }/** 非肯定应答 */
+                    amount_code = this[sureAnsIndex + 1].toInt()/** 故障数 */
+                    var elements = this.slice(sureAnsIndex+2 .. size-1) /** 取故障码数据及填充数据*/
+                    troCodeDataList.addAll(elements)/** 添加到列表 */
+                    return@loop
+                } ?:apply {/** 长帧 */
+                    takeIf { index==0 }?.apply {
+                        index_flag=indexOf(0x10)
+                        val index_sure = indexOf(cmd.plus(0x40.toByte()).toByte())
+                        takeIf { index_sure == -1 }?.apply { return@loop }
+                        amount_code=this[index_sure+1].toInt()
+                        var flow_first = slice(index_sure + 2..size-1)
+                        troCodeDataList.addAll(flow_first)
+
+                    }?:apply {
+                        var flow_else=slice(index_flag+1 .. size-1)
+                        troCodeDataList.addAll(flow_else)
+                    }
+                }
+            }
+        }
+
+        var codeArrays = troCodeDataList.toList().take(amount_code * 2)
+        var tempCodeStr=""
+        for( i in codeArrays.indices step 2){
+            var data_head = codeArrays[i].toInt()
+            var data_end = codeArrays[i+1].toInt()
+            if(data_head==0x00 &&  data_end==0x00){
+                continue
+            }
+            val codeHex=(data_head.shl(8).and(0xffff)).or(data_end.and(0xff))
+
+            try {
+                tempCodeStr = when {
+                    codeHex < 0x4000 -> { // P
+                        String.format("P%04X", codeHex)
+                    }
+                    codeHex < 0x8000 -> { // C
+                        String.format("C%04X", codeHex - 0x4000)
+                    }
+                    codeHex < 0xC000 -> { // B
+                        String.format("B%04X", codeHex - 0x8000)
+                    }
+                    else -> { // U
+                        String.format("U%04X", codeHex - 0xC000)
+                    }
+                }
+                troCodeTextList.add(tempCodeStr)
+            }catch (e: Exception){
+                println("format exception")
+            }
+        }
+
+        return Pair(amount_code, troCodeTextList)
     }
 }
--- app/adtenx/src/test/java/com/xtooltech/adtenx/common/ble/TrobleCodeTest.kt
+++ app/adtenx/src/test/java/com/xtooltech/adtenx/common/ble/TrobleCodeTest.kt
@@ -11,7 +11,7 @@ class TrobleCodeTest{
 
 
     @Test
-    fun readStd(){
+    fun readCanStd(){
         val destruct=DestructCanStd()
         val resp_single: List<ByteArray?> = listOf<ByteArray>(
             byteArrayOf(0x60,0x0A,0x0B,0x08,0x07, 0xE8.toByte(),0x04,0x43,0x01,0x00,0x26,0x00,0x00,0x00)
@@ -30,10 +30,6 @@ class TrobleCodeTest{
         println(result_mutil.second)
     }
 
-    @Test
-    fun readStdExt(){
-        //60 0A 0D 88 18 DA F1 58 04 41 03 00 00 AA AA AA
-    }
 
     @Test
     fun readIso(){
@@ -48,9 +44,30 @@ class TrobleCodeTest{
         var result = deser.parseTrobCode(cmd, data)
         println(result.first.toString() +">>>"+result.second.toString())
         assertEquals(result.first,1)
+    }
 
 
+    @Test
+    fun readCanExt(){
 
+        var cmd:Byte=0x07
+        ObdManger.getIns().currProto= OBD_EXT_CAN
+        val destruct=DestructCanExt()
+//        val resp_single: List<ByteArray?> = listOf<ByteArray>(
+//            byteArrayOf(0x60,0x0A,0x0B,0x08,0x07, 0xE8.toByte(),0x04,0x43,0x01,0x00,0x26,0x00,0x00,0x00)
+//        )
+        val resp_mutil:List<ByteArray?> = listOf(
+            byteArrayOf(0x88.toByte(),0x18, 0xDA.toByte(), 0xF1.toByte(),0x00,0x10,0x0E,0x47,0x06,0x12,0x19,0x12,0x1A),
+            byteArrayOf(0x88.toByte(),0x18, 0xDA.toByte(), 0xF1.toByte(),0x00,0x21,0x12,0x16,0x12,0x18,0x12,0x06,0x12),
+            byteArrayOf(0x88.toByte(),0x18, 0xDA.toByte(), 0xF1.toByte(),0x00,0x22,0x38,0x00,0x00,0x00,0x00,0x00,0x00)
+        )
+//        val result_single=destruct.parseTrobCode(0x03,resp_single)
+//        println(result_single.second)
+//        assertEquals(1,result_single.first)
+
+        val result_mutil=destruct.parseTrobCode(0x07,resp_mutil)
+        assertEquals(6,result_mutil.first)
+        println(result_mutil.second)
     }
 
     @Test
